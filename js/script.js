3b9dbbd4b2fdb8cf873b42ecf0aab9b129d5d002
Bohemian Codes <chavelitaloca@gmail.com>
2017-09-19 15:08:07 +0200

    finished exercise 2.6

---
+++ js/script.js
@@ -0,0 +1,59 @@
+//Twitter Code
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+
+$(document).ready(function(){
+
+// Smooth Scrolling 
+  var $root = $('html, body');
+
+  $('.navbar-nav a').click(function() {
+    var href = $.attr(this, 'href');
+
+    if (href != undefined && href != '#') {
+      $root.animate({
+        scrollTop: $(href).offset().top
+      }, 500, function () {
+        window.location.hash = href;
+      });
+    }
+    return false;
+  });
+
+// Tooltips jQuery
+    $(function () {
+     $('#blog_link').tooltip();
+    });
+
+    $(function () {
+      $('[data-toggle="tooltip"]').tooltip();
+    });
+
+// JS that causes the iframe video to stop when modal is exited
+    $('.sierra_nevada').each(function(){
+        var src = $(this).find('iframe').attr('src');
+
+      $(this).on('click', function(){
+
+        $(this).find('iframe').attr('src', '');
+        $(this).find('iframe').attr('src', src);
+
+      });
+    });
+
+    $("#thankyou").hide();
+
+    $("#form-button").on("click", function () {
+      var comment = $(".message-box").val();
+      console.log(comment);
+      $("#visible-comment").html(comment);
+      $(".message-box").hide();
+      $("#form-button").hide();
+      $("#name").hide();
+      $("#email").hide();
+      $("#thankyou").fadeIn(600)
+      $("#thankyou").html("I have received your message, thank you!");
+      return false;
+    });
+
+
+}); //end document Ready function
