a3d25c85b7f13197ee0659be97da8d3ab73253d1
Bohemian Codes <chavelitaloca@gmail.com>
2017-09-25 19:28:12 +0200

    corrected indentation

--- js/script.js
+++ js/script.js
@@ -6,29 +6,29 @@
 var map;
 var myLatLng = {lat: 52.5230489, lng: 13.5076008}
 
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: myLatLng,
-          zoom: 12,
-          mapTypeId: 'hybrid',
-        });
+	function initMap() {
+			map = new google.maps.Map(document.getElementById('map'), {
+				center: myLatLng,
+				zoom: 12,
+				mapTypeId: 'hybrid',
+			});
 
-        map.setTilt(45);
+			map.setTilt(45);
 
-        var image = 'http://cf.bohemiancodes.com/img/visit.png';
-         var pin = new google.maps.Marker({
-            position: myLatLng,
-            map: map,
-            icon: image,
-            animation: google.maps.Animation.DROP,
-            title: 'Bohemian Codes Office',
-          });
+			var image = 'http://cf.bohemiancodes.com/img/visit.png';
+			 var pin = new google.maps.Marker({
+					position: myLatLng,
+					map: map,
+					icon: image,
+					animation: google.maps.Animation.DROP,
+					title: 'Bohemian Codes Office',
+				});
 
-        pin.addListener('click', function() {
-            map.setZoom(16);
-            map.setCenter(marker.getPosition());
-          });
-      }
+			pin.addListener('click', function() {
+					map.setZoom(16);
+					map.setCenter(pin.getPosition());
+				});
+		}
 
 $(document).ready(function(){
 
@@ -49,84 +49,84 @@ $(document).ready(function(){
 	});
 
 // Tooltips jQuery
-		$(function () {
-		 $('#blog_link').tooltip();
-		});
+	$(function () {
+	 $('#blog_link').tooltip();
+	});
 
-		$(function () {
-			$('[data-toggle="tooltip"]').tooltip();
-		});
+	$(function () {
+		$('[data-toggle="tooltip"]').tooltip();
+	});
 
 // JS that causes the iframe video to stop when modal is exited
-		$('.sierra_nevada').each(function(){
-				var src = $(this).find('iframe').attr('src');
+	$('.sierra_nevada').each(function(){
+			var src = $(this).find('iframe').attr('src');
 
-			$(this).on('click', function(){
+		$(this).on('click', function(){
 
-				$(this).find('iframe').attr('src', '');
-				$(this).find('iframe').attr('src', src);
+			$(this).find('iframe').attr('src', '');
+			$(this).find('iframe').attr('src', src);
 
-			});
 		});
+	});
 
 // Contact-Form jQuery. 
 
-		$("#thankyou").hide();
+	$("#thankyou").hide();
 
-		$("#form-button").on("click", function () {
-			var comment = $(".message-box").val();
-			console.log(comment);
+	$("#form-button").on("click", function () {
+		var comment = $(".message-box").val();
+		console.log(comment);
 
-			if (comment === "") {
-				$(".message-box").css("border", "1.5px solid red");
-				} else {
-				$("#visible-comment").html(comment);
-				$(".message-box").hide();
-				$("#form-button").hide();
-				$("#name").hide();
-				$("#email").hide();
-				$("#thankyou").fadeIn(600);
-				$("#char-count").hide();
-				$("#thankyou").html("I have received your message, thank you!");
-			}
-			return false;
-		});
+		if (comment === "") {
+			$(".message-box").css("border", "1.5px solid red");
+		} else {
+			$("#visible-comment").html(comment);
+			$(".message-box").hide();
+			$("#form-button").hide();
+			$("#name").hide();
+			$("#email").hide();
+			$("#thankyou").fadeIn(600);
+			$("#char-count").hide();
+			$("#thankyou").html("I have received your message, thank you!");
+		}
+		return false;
+	});
 
-		$(".message-box").on("keyup", function () {
-			console.log("keyup happened");
-			var charCount = $(".message-box").val().length;
-			$("#char-count").html("Characters: " + charCount);
+	$(".message-box").on("keyup", function () {
+		console.log("keyup happened");
+		var charCount = $(".message-box").val().length;
+		$("#char-count").html("Characters: " + charCount);
 
-			if (charCount > 100) {
-				$("#char-count").css({"color": "red", "font-weight": "bold"});
-			} else {
-				$("#char-count").css("color", "#FCFCFC");
+		if (charCount > 100) {
+			$("#char-count").css({"color": "red", "font-weight": "bold"});
+		} else {
+			$("#char-count").css("color", "#FCFCFC");
 
-			}
-		});
+		}
+	});
 
 // Work Section - populating work samples from work.js file
 
-		for(var i = 0; i < works.length; ++i ) {
-			$(".work-row").append(' \
-					<div class="col-md-3 col-sm-6 work-box"> \
-					<a href="' + works[i].url +'" class="work-img">\
-					<img class="img-responsive" src="' + works[i].pic + '">\
-					<span class="info" id="' + works[i].id + '"><p class="proj-title"></p>' + works[i].title + ' </span>\
-					</a>\
-					</div>\
-				');
+	for(var i = 0; i < works.length; ++i ) {
+		$(".work-row").append(' \
+				<div class="col-md-3 col-sm-6 work-box"> \
+				<a href="' + works[i].url +'" class="work-img">\
+				<img class="img-responsive" src="' + works[i].pic + '">\
+				<span class="info" id="' + works[i].id + '"><p class="proj-title"></p>' + works[i].title + ' </span>\
+				</a>\
+				</div>\
+			');
 
-		}; //end for-loop
+	}; //end for-loop
 
 // Fade in and out of title when hovering over images
-		$(".work-img").mouseenter( function() {
-			$(".info", this).fadeIn();
-		});
+	$(".work-img").mouseenter( function() {
+		$(".info", this).fadeIn();
+	});
 
-		$(".work-img").mouseleave( function() {
-			$(".info", this).fadeOut();
-		});
+	$(".work-img").mouseleave( function() {
+		$(".info", this).fadeOut();
+	});
 			
 
 
